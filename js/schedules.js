162e111924fdae94b1dfc1e01e1891a5e8bd6396
Daniel Alonso <daniel.colmenares.ar@gmail.com>
2022-01-16 18:22:31 -0300

    Seccion Horarios: Proceso de imprimir datos en la tabla de los horarios completo; pendiente por capturar y validar datos de la hora

--- js/schedules.js
+++ js/schedules.js
@@ -1,3 +1,4 @@
+//DECLARACION DE CLASE
 class Schedule {
     constructor(scheduleName, checkInTime, departureTime) {
         this.scheduleName = scheduleName;
@@ -6,30 +7,108 @@ class Schedule {
     }
 }
 
+//FUNCION QUE CREA UN NUEVO REGISTRO EN LA LISTA DE RUTAS
 let scheduleList = [];
-let addSchedule = (scheduleNameInput,scheduleCheckInInput,scheduleDepartureInput) => {
-    let newSchedule = new Schedule(scheduleNameInput,scheduleCheckInInput,scheduleDepartureInput);
+let addSchedule = (scheduleNameValue,scheduleCheckInValue,scheduleDepartureValue) => {
+    let newSchedule = new Schedule(scheduleNameValue,scheduleCheckInValue,scheduleDepartureValue);
     scheduleList.push(newSchedule);
 }
 
-let scheduleDataSet = '[ {"scheduleName":"Primero Mañana", "scheduleCheckIn":"07:45", "scheduleDeparture":"08:00" }, {"scheduleName":"Segundo Mañana", "scheduleCheckIn":"08:25", "scheduleDeparture":"08:40"},{"scheduleName":"Primero Tarde", "scheduleCheckIn":"14:15", "scheduleDeparture":"14:30"} ]';
-let scheduleData = JSON.parse( scheduleDataSet );
+//VALIDAMOS SI EXISTE INFORMACION EN EL STORAGE
+let scheduleData = JSON.parse(sessionStorage.getItem('scheduleDataSetJSON'));
 
-for (let index = 0; index < scheduleData.length; index++) {
-
-    let scheduleNameInput = scheduleData[index].scheduleName;
-    let scheduleCheckInInput = scheduleData[index].scheduleCheckIn;
-    let scheduleDepartureInput = scheduleData[index].scheduleDeparture;
-
-    addSchedule(scheduleNameInput,scheduleCheckInInput,scheduleDepartureInput);
+if( scheduleData == null ){
+    let scheduleDataSet = '[ {"scheduleName":"Primero Mañana", "scheduleCheckIn":"07:45", "scheduleDeparture":"08:00" }, {"scheduleName":"Segundo Mañana", "scheduleCheckIn":"08:25", "scheduleDeparture":"08:40"},{"scheduleName":"Primero Tarde", "scheduleCheckIn":"14:15", "scheduleDeparture":"14:30"} ]';
+    sessionStorage.setItem('scheduleDataSetJSON', scheduleDataSet);
 }
 
-for (let index = 0; index < scheduleList.length; index++) {
-    let tableSchedule = document.querySelector('#table-schedule').querySelector('tbody');
-    let newRowElement = document.createElement('tr');
-    let newColumnElement = document.createElement('td');
+//FUNCION PARA TRAER INFORMACION ALMACENADA EN EL STORAGE
+let loadDataSet = () => {
+    scheduleData = JSON.parse(sessionStorage.getItem('scheduleDataSetJSON'));
+    scheduleList = [];
 
-    tableSchedule.appendChild(newRowElement);
-    newRowElement.appendChild(newColumnElement);
-    newColumnElement.textContent = scheduleList[index].scheduleName;
+    for (let index = 0; index < scheduleData.length; index++) {
+        scheduleNameData = scheduleData[index].scheduleName;
+        scheduleCheckInData = scheduleData[index].scheduleCheckIn;
+        scheduleDepartureData = scheduleData[index].scheduleDeparture;
+    
+        addSchedule(scheduleNameData,scheduleCheckInData,scheduleDepartureData);
+    }
+}
+
+//FUNCION PARA REGISTRAR NUEVA INFORMACION EN EL STORAGE
+let addScheduleToData = (addName) => {
+    //Agregamos registro al ScheduleList
+    addSchedule(addName, "00:00", "00:00");
+
+    newData = JSON.parse(sessionStorage.getItem('scheduleDataSetJSON'));
+    newData.push(scheduleList[scheduleList.length-1]);
+    sessionStorage.setItem('scheduleDataSetJSON', JSON.stringify(newData));
+    //Cargamos al storage
+    loadDataSet();
+}
+
+//FUNCION PARA IMPRIMIR LOS VALORES EN LA TABLA
+let printScheduleTable = () => {
+    for (let index = 0; index < scheduleList.length; index++) {
+        let tableSchedule = document.querySelector('#table-schedule').querySelector('tbody');
+        let newRowElement = document.createElement('tr');
+        let newColumnElement = document.createElement('td');
+    
+        tableSchedule.appendChild(newRowElement);
+        newRowElement.appendChild(newColumnElement);
+        newColumnElement.textContent = scheduleList[index].scheduleName;
+    }
+}
+
+loadDataSet();
+printScheduleTable();
+
+//FUNCION PARA CONSEGUIR LA INFORMACION DEL FORMULARIO
+let getScheduleFormData = () => {
+    //Validamos el Nombre del horario
+    let scheduleNameInput = document.querySelector("#scheduleNameInput").value;
+    let scheduleCheckInInput =  document.querySelector("#scheduleCheckInInput").value;
+    let scheduleDepartureInput = document.querySelector("#scheduleDepartureInput").value;
+    
+    let scheduleNameValidated;
+    let scheduleCheckInValidated;
+    let scheduleDepartureValidated;
+
+    //Validamos que el contenido del input Name sea valido
+    if(scheduleNameInput != "" && scheduleNameInput != null && scheduleNameInput != undefined ){
+        //Comparamos el nombre con todos los guardados con anterioridad
+        let scheduleNameExist = false;
+        for (let index = 0; index < scheduleList.length; index++) {
+            if(scheduleList[index].scheduleName == scheduleNameInput ){
+                scheduleNameExist = true;
+            }
+        }
+
+        switch(scheduleNameExist){
+            case true:
+                alert("El nombre ya existe");
+                break;
+
+            case false:
+                scheduleNameValidated = scheduleNameInput;
+                break;
+        }
+    }
+    else {
+        alert("Nombre Invalido")
+    }
+
+    if(scheduleNameValidated != undefined){
+        addScheduleToData(scheduleNameValidated);
+
+        //Imprimo el valor en la tabla
+        let tableSchedule = document.querySelector('#table-schedule').querySelector('tbody');
+        let newRowElement = document.createElement('tr');
+        let newColumnElement = document.createElement('td');
+        tableSchedule.appendChild(newRowElement);
+        newRowElement.appendChild(newColumnElement);
+        newColumnElement.textContent = scheduleList[scheduleList.length-1].scheduleName;
+    }
+
 }
